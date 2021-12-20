56c7e90b1b484fb5a4f07a17e088d45954f7bb5c
GlenDu <glen_llaci@yahoo.com>
2021-12-20 11:04:28 +0100

    partials views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users, path:  'custom/user', :only => [:show] 
   resources :user_combats
   resources :combats
-  devise_for :users
+  #devise_for :users
   resources :comptes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
