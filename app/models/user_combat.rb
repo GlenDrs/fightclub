3fa3ffca87a73d8d98b98909548dc7621fa3547a
GlenDu <glen_llaci@yahoo.com>
2021-12-25 17:40:59 +0100

    User validations for combat participation

--- app/models/user_combat.rb
+++ app/models/user_combat.rb
@@ -2,14 +2,20 @@ class UserCombat < ApplicationRecord
   belongs_to :user
   belongs_to :combat
 
-  #validates_uniqueness_of :combat_id, :scope => :user_id
   validate :player_count_validation
+  validate :unique_player
 
   private
 
   def player_count_validation
     if combat.user_combats.count >= 2
       errors.add(:combat,"must have length at most two")
+    end
+  end
+
+  def unique_player
+    if combat.user_combats.map {|uc| uc.user_id }.include?(user.id)
+      errors.add(:user,"You can't fight yourself coco!")
     end
   end
 end
